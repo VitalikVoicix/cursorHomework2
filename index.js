7ca1112d79e817fa0c1fb6ef7f0c652c5bfb7a15
VitalikVoicix <voicix1502@gmail.com>
2020-07-20 01:02:24 +0300

    one change

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let userInteger1 = Number(prompt("Enter your first number: "));
-    while(userInteger1 !== parseInt(userInteger1)){
+    while(userInteger1 !== parseInt(userInteger1)|| userInteger1 == " "){
 
     userInteger1 = Number(prompt("Your number is wrong.Enter your first number: "));
 }
 
 let userInteger2 = Number(prompt("Enter your second number: "));
-     while(userInteger2 !== parseInt(userInteger2)){
+     while(userInteger2 !== parseInt(userInteger2) ||  userInteger2 < userInteger1 || userInteger2 == " " ) {
 
-    userInteger2 = Number(prompt("Your number is wrong.Enter your second number: "))
+    userInteger2 = Number(prompt("Your number is wrong.Enter your second number: "));
 }
 
 let passEvenNumbers = confirm("Do we need to skip even numbers?")
